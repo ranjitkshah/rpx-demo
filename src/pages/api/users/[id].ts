b7d501df03bfc2d5f86736cc791ebc96f7b177e7
Charlie Sparks <sparksfullstack@gmail.com>
2023-04-22 15:20:51 -0400

    Charlie | Minor updates to routes made

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -8,29 +8,37 @@ const handler = withAuth(async (req: NextApiRequest, res: NextApiResponse) => {
 	const { method } = req
 	const { id } = req.query as { id: string }
 
-	if (method !== APIMethods.GET || !id) {
+	if (!id) {
 		return res.status(404).json({ status: APIStatuses.ERROR, type: GeneralAPIResponses.INVALID_REQUEST_TYPE })
 	}
 
-	try {
-		const db = getFirestore(firebase_app)
-		const collectionRef = doc(db, CollectionNames.USERS, id)
-		const documentSnapshot = await getDoc(collectionRef)
+	if (method === APIMethods.GET) {
+		try {
+			const db = getFirestore(firebase_app)
+			const collectionRef = doc(db, CollectionNames.USERS, id)
+			const documentSnapshot = await getDoc(collectionRef)
 
-		if (documentSnapshot.exists()) {
-			const document = documentSnapshot.data()
-			res.status(200).json({
-				status: APIStatuses.SUCCESS,
-				type: DocumentResponses.DATA_FOUND,
-				data: { user: document }
-			})
-		} else {
-			console.error('e', DocumentResponses.DATA_NOT_FOUND)
-			res.status(400).json({ status: APIStatuses.ERROR, type: DocumentResponses.DATA_NOT_FOUND })
+			if (documentSnapshot.exists()) {
+				const document = documentSnapshot.data()
+				return res.status(200).json({
+					status: APIStatuses.SUCCESS,
+					type: DocumentResponses.DATA_FOUND,
+					data: { user: { id, ...document } }
+				})
+			} else {
+				console.error('e', DocumentResponses.DATA_NOT_FOUND)
+				return res.status(400).json({ status: APIStatuses.ERROR, type: DocumentResponses.DATA_NOT_FOUND })
+			}
+		} catch (e) {
+			console.error('e', e)
+			return res.status(400).json({ status: APIStatuses.ERROR, type: GeneralAPIResponses.FAILURE, data: { error: e } })
 		}
-	} catch (e) {
-		console.error('e', e)
-		res.status(400).json({ status: APIStatuses.ERROR, type: GeneralAPIResponses.FAILURE, data: { error: e } })
+	} else if (method === APIMethods.PATCH) {
+		// update logic goes here
+	} else if (method === APIMethods.DELETE) {
+		// delete logic goes here
+	} else {
+		return res.status(404).json({ status: APIStatuses.ERROR, type: GeneralAPIResponses.INVALID_REQUEST_TYPE })
 	}
 })
 
