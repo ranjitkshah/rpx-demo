071f7db0f5757fd57aa4cf15d454575636ac5df9
Stephen Fischer <sfischer1982@gmail.com>
2023-04-30 01:43:18 -0400

    Fix uploader styling

--- src/components/general/Uploader.tsx
+++ src/components/general/Uploader.tsx
@@ -1,12 +1,25 @@
-import React, { ChangeEventHandler, DragEventHandler, useState } from 'react'
+import React, { ChangeEventHandler, DragEventHandler, SVGProps, useState } from 'react'
 
 type UploaderProps = {
 	onUpload: (file: File | null) => void
 }
 
+const UploadArrow = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="210.895 270.192 60.696 120.115"
+  >
+    <path
+      fill="#FFF"
+      stroke="#FFF"
+      d="M237.977 385.793l-.464-44.92-24.499.253 27.327-65.286 28.67 64.707-24.498.253.464 44.92-7 .073z"
+    ></path>
+  </svg>
+)
+
 // TODO: handle errors
 const Uploader: React.FC<UploaderProps> = ({ onUpload }) => {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+	const [selectedFile, setSelectedFile] = useState<File | null>(null)
 	const [dragging, setDragging] = useState(false)
 
 	const handleDragOver: DragEventHandler = (e) => {
@@ -23,20 +36,20 @@ const Uploader: React.FC<UploaderProps> = ({ onUpload }) => {
 		e.preventDefault()
 		setDragging(false)
 		const file = e.dataTransfer.files[0]
-    setSelectedFile(file)
+		setSelectedFile(file)
 		onUpload(file)
 	}
 
 	const handleInputChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
 		const file = target.files?.[0]
 		if (file) {
-      setSelectedFile(file)
+			setSelectedFile(file)
 			onUpload(file)
 		}
 	}
 
 	const handleClearClick = () => {
-    setSelectedFile(null)
+		setSelectedFile(null)
 		onUpload(null)
 	}
 
@@ -55,8 +68,9 @@ const Uploader: React.FC<UploaderProps> = ({ onUpload }) => {
 						className="rounded-full object-cover m-auto"
 					></img>
 				) : (
-					<div className="w-36 h-36 bg-indigo-300 rounded-full flex justify-center items-center text-center p-5 m-auto">
-						Drag file to upload
+					<div className="w-36 h-36 bg-indigo-200 rounded-full flex flex-col justify-center items-center text-center p-5 m-auto">
+						<UploadArrow />
+						<p className="whitespace-normal text-2xl tracking-wide">Drag file to upload</p>
 					</div>
 				)}
 			</div>
