71a5e365d342750e77a5ba8ede4c7cfd5bae6ed4
Charlie Sparks <sparksfullstack@gmail.com>
2023-05-01 01:25:06 -0400

    Charlie | Fix for bad sizing of elements on main page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { SignIn } from '@clerk/nextjs'
 import Layout from '@/components/Layout/secondaryLayout'
 
+// TODO: Fix the auth pages!! They aren't redirectly properly
 const SignInPage = () => (
 	<div className="h-[85vh] w-[100vw] flex justify-center items-center">
 		<SignIn path="/auth/sign-in" routing="path" signUpUrl="/auth/sign-up" />
