878c44f935f6e1cd7a16909a1591d35bea7d4fbf
Charlie Sparks <sparksfullstack@gmail.com>
2023-05-01 20:36:03 -0400

    Charlie | Added service worker setup

--- public/registerServiceWorker.js
+++ public/registerServiceWorker.js
@@ -1,12 +1,16 @@
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/sw.js').then(
-			(registration) => {
-				console.log('Service Worker registration successful with scope: ', registration.scope)
-			},
-			(err) => {
-				console.log('Service Worker registration failed: ', err)
-			}
-		)
-	})
+if (typeof window !== 'undefined') {
+	// Check if the browser supports service workers
+	if ('serviceWorker' in navigator) {
+		// Register the service worker
+		window.addEventListener('load', function () {
+			navigator.serviceWorker.register('/service-worker.js').then(
+				(registration) => {
+					console.log('Service Worker registered with scope: ', registration.scope)
+				},
+				(err) => {
+					console.error('Service Worker registration failed: ', err)
+				}
+			)
+		})
+	}
 }
